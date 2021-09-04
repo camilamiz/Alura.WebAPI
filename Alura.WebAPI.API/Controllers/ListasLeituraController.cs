518fea40e6ae73cfe966c1f4acfbc21e0d6409e0
camilamiz <camila.yuri.m@gmail.com>
2021-09-04 13:55:49 -0300

    Extract Api logic to another project

---
+++ Alura.WebAPI.API/Controllers/ListasLeituraController.cs
@@ -0,0 +1,63 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.ListaLeitura.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ListasLeituraController : ControllerBase 
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public ListasLeituraController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+        private Lista CriaLista(TipoListaLeitura tipo)
+        {
+            return new Lista
+            {
+                Tipo = tipo.ParaString(),
+                Livros = _repo.All
+                .Where(l => l.Lista == tipo)
+                .Select(l => l.ToApi())
+                .ToList()
+            };
+        }
+
+        [HttpGet]
+        public IActionResult TodasListas()
+        {
+            Lista paraLer = CriaLista(TipoListaLeitura.ParaLer);
+            Lista lendo = CriaLista(TipoListaLeitura.Lendo);
+            Lista lidos = CriaLista(TipoListaLeitura.Lidos);
+
+            var colecao = new List<Lista> { paraLer, lendo, lidos };
+            return Ok(colecao);
+        }
+
+        [HttpGet("{tipo}")]
+        public IActionResult Recuperar(TipoListaLeitura tipo)
+        {
+            //um exemplo de como é feita a verificação de autorização a cada requisição
+            //mandamos no header Authorization (key) e 123 (value),
+            //não tem consulta a nenhuma base de dados (como é feito com cookies e session id), sem acoplamento com servidor
+            //var header = this.HttpContext.Request.Headers;
+            //if (!header.ContainsKey("Authorization") || !(header["Authorization"] == "123"))
+            //{
+            //    return StatusCode(401);
+            //}
+
+            var lista = CriaLista(tipo);
+            return Ok(lista);
+        }
+    }
+}
---
+++ Alura.WebAPI.API/Startup.cs
@@ -0,0 +1,86 @@
+using Alura.ListaLeitura.Api.Formatters;
+using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<LeituraContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("ListaLeitura"));
+            });
+            services.AddTransient<IRepository<Livro>, RepositorioBaseEF<Livro>>();
+
+            services.AddMvc(options => {
+                options.OutputFormatters.Add(new LivroCSVFormatter());
+            }).AddXmlSerializerFormatters();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("valid-alura-webapi-authentication")),
+                    ClockSkew = System.TimeSpan.FromMinutes(5),
+                    ValidIssuer = "Alura.WebApp",
+                    ValidAudience = "Insomnia"
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+        }
+    }
+}
